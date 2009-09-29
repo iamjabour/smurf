79c337c28b0a42f128860c25d02248e005da919a
Iam <iamjabour@gmail.com>
2009-09-29 17:15:13 -0300

    Mudancas na distancia de edicao para suportar custos diferentes para as operacoes

--- sources/python/projetoProgramacao/utils/distances.py
+++ sources/python/projetoProgramacao/utils/distances.py
@@ -1,22 +1,29 @@
-def stringDistance(first, second):
+def stringDistance(first, second, cost={'a':1, 'd':1, 'c':1}):
     """Find the Levenshtein distance between two strings."""
-    if len(first) > len(second):
-        first, second = second, first
-    if len(second) == 0:
-        return len(first)
+
+    if len(second) == 0 or len(first) == 0:
+        return max(len(first),len(second))
+
     first_length = len(first) + 1
     second_length = len(second) + 1
     distance_matrix = [[0] * second_length for x in range(first_length)]
-    for i in range(first_length):
-       distance_matrix[i][0] = i
-    for j in range(second_length):
-       distance_matrix[0][j]=j
+
+    for i in xrange(0, first_length):
+        distance_matrix[i][0] = i * cost['d']
+        for j in xrange(1, second_length):
+            if i == 0:
+               distance_matrix[i][j] = j * cost['a']
+            else:
+                distance_matrix[i][j] = 999999
+
     for i in xrange(1, first_length):
-        for j in range(1, second_length):
-            deletion = distance_matrix[i-1][j] + 1
-            insertion = distance_matrix[i][j-1] + 1
+        for j in xrange(1, second_length):
+            deletion = distance_matrix[i-1][j] + cost['d']
+            insertion = distance_matrix[i][j-1] + cost['a']
             substitution = distance_matrix[i-1][j-1]
             if first[i-1] != second[j-1]:
-                substitution += 1
-            distance_matrix[i][j] = min(insertion, deletion, substitution)
-    return distance_matrix[first_length-1][second_length-1]
+                substitution += cost['c']
+            distance_matrix[i][j] = min(insertion, deletion, substitution, \
+                distance_matrix[i][j])
+
+    return distance_matrix[-1][-1]
