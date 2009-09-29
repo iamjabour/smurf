6588140f8b8668e636ceda018a21f3281f2095c9
Iam <iamjabour@gmail.com>
2009-09-29 17:16:50 -0300

    Adicionando os dois primeiros testes unitarios do projeto.

---
+++ sources/python/projetoProgramacao/utils/test_distances.py
@@ -0,0 +1,41 @@
+import unittest
+import utils.distances as distances
+
+class TestDistances(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_stringDistance(self):
+        print '\n - Test: utils.distances.stringDistance()'
+        add = 1
+        change = 2
+        delete = 4
+        cost = { 'a': add, 'd': delete, 'c': change }
+
+        dist = distances.stringDistance('aa','aba', cost)
+        self.assertEqual(dist, add, '1: %d!=%d' % (dist, change))
+
+        dist = distances.stringDistance('a', 'ab', cost)
+        self.assertEqual(dist, add, '2: %d!=%d' % (dist, change))
+
+        dist = distances.stringDistance('a', 'ba', cost)
+        self.assertEqual(dist, add, '3: %d!=%d' % (dist, change))
+
+        dist = distances.stringDistance('a', 'b', cost)
+        self.assertEqual(dist, change, '4: %d!=%d' % (dist, change))
+
+        dist = distances.stringDistance('ba', 'a', cost)
+        self.assertEqual(dist, delete, '5: %d!=%d' % (dist, change))
+
+        dist = distances.stringDistance('aba', 'aa', cost)
+        self.assertEqual(dist, delete, '6: %d!=%d' % (dist, change))
+
+    def test_2(self):
+#        print '\n - Test - Nada'
+        pass
+
+if __name__ == '__main__':
+    print "Test: utils.distances"
+    unittest.main()
+
---
+++ sources/python/projetoProgramacao/utils/test_parsedom.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import utils.parsedom as parsedom
+import unittest
+
+class TestParseDom(unittest.TestCase):
+    def setUp(self):
+        self.parser = parsedom.ParseDom()
+
+    def test_convertStringToUTF8(self):
+        print '\t - Test: utils.parsedom._convertStringToUTF8()'
+        ret = self.parser._convertStringToUTF8('ã').strip()
+        self.assertEqual(ret, 'ã', '1: String can not be converted!')
+
+        # testa se uma string dada em unicode (Á) pode ser convertida para UTF8
+        ret = self.parser._convertStringToUTF8('\xc1').strip()
+        self.assertEqual(ret, u'\xc1'.encode('utf8'))
+
+    def test_parse(self):
+        print '\t - Test: utils.parsedom.parse()'
+        ret = self.parser.parse("<html><body>Hi</body></html>")
+        self.assert_(ret != None)
+
+        ret = self.parser.parse("<html><body>Ho")
+        self.assert_(ret != None)
+
+if __name__ == '__main__':
+    print "Test: utils.parsedom"
+    unittest.main()
