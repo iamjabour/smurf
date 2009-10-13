123e469820ddc0db65dd7de60633fc22f73b3743
Iam <iamjabour@gmail.com>
2009-10-13 12:10:18 -0300

    Primeira ferramenta para rodar o projeto com algum objetivo.

---
+++ sources/python/eri/apps/fastbechmark.py
@@ -0,0 +1,45 @@
+import sys
+from eri.utils.dynamicimport import *
+
+if __name__ == '__main__':
+    import os
+    import optparse
+
+    import eri.extractors
+    modules = eri.extractors.modules
+
+    #create an option parser
+    usage  = """%(prog)s [options] <extractor> <file> [file ...]
+  <extractor>\t\tUser an module name [%(modules)s]
+  <file>     \t\tOne or any path to html file(s)""" % \
+  {'prog':sys.argv[0], 'modules': '| '.join(modules)}
+
+    parser = optparse.OptionParser(usage=usage)
+
+    parser.add_option('-o', '--output', default='stdout', \
+      dest='output', \
+      help="Create and redirect stdout to file")
+
+    (opt, args) = parser.parse_args()
+
+    if len(sys.argv) < 3:
+        raise SystemExit(parser.print_help())
+
+
+    extractor = dimport("eri.extractors.%s" % args[0].lower(), args[0])
+
+    from eri.utils.parsedom import ParseDom
+
+    filePath = args[1]
+
+    if filePath[0:4] == "http":
+        import urllib
+        htmlString = urllib.urlopen(filePath).read()
+    else:
+        htmlString = open(filePath, 'r').read()
+
+    parser = ParseDom()
+    dom = parser.parse(htmlString)
+    r = extractor.process(dom)
+
+    print r
