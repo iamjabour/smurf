792172fa220bfeece244a8243d5cc95ea5d9e4a8
Iam <iamjabour@gmail.com>
2010-02-03 17:17:25 -0200

    Adicionando ferramenta que ira baixar e anotar o corpus. Atualmente somento o modulo do submarino esta parcialmente pronto, falta anotar antes de salvar.

---
+++ sources/python/anotador/comercioeletronico/getcorpus.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+corpus=$1
+DIR=$(pwd)
+
+#criando a pasta de download
+if [ ! -d "download" ] 
+then
+	echo "Criando a pasta download para armazenar o corpus"
+	$(mkdir "$DIR/download")
+fi
+
+DIR="$DIR/download"
+
+#criando a pasta para guardar o corpus
+if [ -d $1 ]
+then
+	echo "Corpus desejada já existe, tente outro corpus ou delete o corpus existente"
+#	exit
+else
+	$(mkdir "$DIR/$corpus")
+	$(mkdir "$DIR/proof_$corpus")
+fi
+
+
+#fazendo download dos arquivos descritos no corpus.txt
+i=0
+while read line
+do
+	i=$(($i+1));
+	printf "downloading %03d document\n" $i
+	echo $line
+	f=${line#*submarino.com.br/menu/}
+	file=${f/\//.}
+
+	out="$DIR/$corpus/$file"
+	proof="$DIR/proof_$corpus/$file"
+
+	if [ -e $out.html ]
+	then
+		echo -e "\nERROR: Documento ja faz parte do corpus: $line\n"
+	else
+		echo $out
+		$(wget -o $out.log -v -O $out.html $line)
+		if [ ! -e $out.html ]
+		then
+			echo -e "\nERROR: Documento não foi pego no wget: $out"
+		fi
+
+		$(python $corpus.py $line $proof.html)
+
+		echo
+	fi
+done < $corpus.txt
+
+echo "foram processadas $i linhas"
