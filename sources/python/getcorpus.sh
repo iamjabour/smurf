a847540608c9e73a54b927c9bf0a7775410ef976
Iam Jabour <iamjabour@gmail.com>
2012-02-09 18:37:45 +1300

    Add old file to keep it for reference

---
+++ sources/python/getcorpus.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+corpus=$1
+DIR=$(pwd)
+
+#criando a pasta de download
+if [ ! -d "download" ] 
+then
+	echo "Criando a pasta download para armazenar o corpus"
+	$(mkdir "$DIR/download")
+fi
+
+DIR="$DIR/download"
+
+#criando a pasta para guardar o corpus
+if [ -d $1 ]
+then
+	echo "Corpus desejada já existe, tente outro corpus ou delete o corpus existente"
+#	exit
+else
+	$(mkdir "$DIR/$corpus")
+	$(mkdir "$DIR/proof_$corpus")
+fi
+
+echo $corpus
+
+#fazendo download dos arquivos descritos no corpus.txt
+i=0
+while read line
+do
+	if [ ! $line ]
+	then
+		continue
+	fi
+
+	i=$(($i+1));
+	printf "downloading %03d document\n" $i
+	echo $line
+	f=${line#*/}
+	if [ $corpus == "submarino" ]
+	then
+		f=${line#*submarino.com.br/menu/}
+	elif [ $corpus == "bestbuy" ]
+	then
+		f=${line#*?id=}
+	elif [ $corpus == "mysimon" ]
+	then
+		f=${line#*compare-prices/}
+	elif [ $corpus == "dealextreme" ]
+	then
+		f=${line#*products.dx/}
+	elif [ $corpus == "ebay" ]
+	then
+		f=${line#*ebay.com/}
+		f=${f%/*}
+	fi
+	if [ $corpus == "americanas" ]
+	then
+		f=${line#*americanas.com.br/}
+		f=${f#*/}
+	elif [ $corpus == "target" ]
+	then 
+		f=${line#*node=}
+	elif [ $corpus == "circuitcity" ]
+	then
+		f=${f#*CatId=}
+		f=${f%&name*}
+	fi
+
+
+
+	file=${f/\//.}
+
+	out="$DIR/$corpus/$file"
+	proof="$DIR/proof_$corpus/$file"
+
+	if [ -e $out.html ]
+	then
+		echo -e "\nERROR: Documento ja faz parte do corpus: $line\n"
+	else
+		echo $out
+		$(wget -o $out.log -v -O $out.html $line)
+		if [ ! -e $out.html ]
+		then
+			echo -e "\nERROR: Documento não foi pego no wget: $out"
+		fi
+
+		$(python $corpus.py $line $proof.html)
+
+		echo
+	fi
+done < $corpus.txt
+
+echo "foram processadas $i linhas"
